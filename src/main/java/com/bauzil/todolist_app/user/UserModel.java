c7418fa87e7ff84d215690fb302de89f1d0db9de
Andre Bauzil <andregbauzil@gmail.com>
2023-10-13 17:22:14 -0300

    Criação da classe do usuário utilizada para criação e arquivo controller para gerenciar o caminho da rota de POST para cadastro de usuários, além de notação com @Data do LomBok.

---
+++ src/main/java/com/bauzil/todolist_app/user/UserModel.java
@@ -0,0 +1,12 @@
+package com.bauzil.todolist_app.user;
+
+import lombok.Data;
+
+@Data
+public class UserModel {
+    // Atributos
+    private String username;
+    private String name;
+    public String password;
+
+}
