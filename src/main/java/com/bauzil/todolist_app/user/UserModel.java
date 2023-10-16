7fac1aea1f7a5fd839ca2a78c25f888b3ab97d4d
Andre Bauzil <andregbauzil@gmail.com>
2023-10-16 15:21:59 -0300

    Adição de notações para definir o ID do user e autogenerar em formado UUID, adição de unique na coluna de username para não repetir, além de adição de timeStamp para saber quando foi criado

--- src/main/java/com/bauzil/todolist_app/user/UserModel.java
+++ src/main/java/com/bauzil/todolist_app/user/UserModel.java
@@ -1,12 +1,29 @@
 package com.bauzil.todolist_app.user;
 
+import java.util.UUID;
+import java.time.LocalDateTime;
+
+import org.hibernate.annotations.CreationTimestamp;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
 import lombok.Data;
 
 @Data
+@Entity(name = "tb_users")
 public class UserModel {
     // Atributos
+    @Id
+    @GeneratedValue(generator = "UUID")
+    private UUID id;
+    
+    @Column(unique = true)
     private String username;
     private String name;
     public String password;
 
+    @CreationTimestamp
+    private LocalDateTime createdAt;
 }
